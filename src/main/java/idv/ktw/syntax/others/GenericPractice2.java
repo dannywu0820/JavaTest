99fe99f42b5bb3f4805511480d43b387dffff9f7
DannyWu <livebetterlife0820@gmail.com>
2021-12-14 12:00:57 +0800

    [feature] learn how to use generics to implement quick sort

--- src/main/java/idv/ktw/syntax/others/GenericPractice2.java
+++ src/main/java/idv/ktw/syntax/others/GenericPractice2.java
@@ -13,6 +13,12 @@ import java.util.Vector;
 public class GenericPractice2 {
 	public static void main(String[] args) {
 		demoBoundedGenerics();
+		
+		String[] words = {"B", "X", "A", "M", "F", "W", "O"};
+		GenericPractice2.sort(words);
+		for(String word: words) {
+			System.out.printf("%s, ", word);
+		}
 	}
 	
 	static void demoBoundedGenerics() {
@@ -24,6 +30,36 @@ public class GenericPractice2 {
 		new CollectionGeneric<>(new LinkedHashSet()).show();
 		new CollectionGeneric<>(new TreeSet()).show();
 	}
+	
+	public static <T extends Comparable> void sort(T[] array) {
+        sort(array, 0, array.length-1);
+    }
+    
+    private static <T extends Comparable> void sort(T[] array, int left, int right) {
+        if(left < right) { 
+            int q = partition(array, left, right); 
+            sort(array, left, q-1); 
+            sort(array, q+1, right); 
+        } 
+    }
+    
+    private static <T extends Comparable> int partition(T[] array, int left, int right) {  
+        int i = left - 1; 
+        for(int j = left; j < right; j++) {
+            if(array[j].compareTo(array[right]) <= 0) {
+                i++; 
+                swap(array, i, j); 
+            } 
+        }
+        swap(array, i+1, right); 
+        return i + 1; 
+    } 
+
+    private static <T> void swap(T[] array, int i, int j) {
+        T t = array[i]; 
+        array[i] = array[j]; 
+        array[j] = t;
+    }
 }
 
 //bounded generics
@@ -36,7 +72,7 @@ class CollectionGeneric <T extends Collection> {
 	
 	public void show() {
 		System.out.println(this.collection.getClass().getName());
-	}	
+	}
 }
 
 //multiple-bounded generics
