3d5a2e05c2c79a6b44d2885b754ec6b6572fb739
DannyWu <livebetterlife0820@gmail.com>
2021-12-15 18:04:38 +0800

    [feature] learn how to sort either by comparable or comparator

--- src/main/java/idv/ktw/syntax/classes/ComparablePractice.java
+++ src/main/java/idv/ktw/syntax/classes/ComparablePractice.java
@@ -2,6 +2,7 @@ package idv.ktw.syntax.classes;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class ComparablePractice {
@@ -9,13 +10,18 @@ public class ComparablePractice {
 		List people = new ArrayList<>();
 		// List<Person> people = new ArrayList<>(); // compile error
 		people.add(new Person("Danny", "Wu", 3));
-		people.add(new Person("Jason", "Kuan", 2));
-		people.add(new Person("Frank", "Ting", 1));
+		people.add(new Person("J", "Kuan", 2));
+		people.add(new Person("Luke", "Ting", 1));
 		Collections.sort(people);
+		Collections.sort(people, new MyComparator());
+		
+		for(Person p: (List<Person>)people) {
+			System.out.println(p);
+		}
 	}
 }
 
-class Person {
+class Person implements Comparable<Person> {
 	private String firstName;
 	private String lastName;
 	private Integer zipCode;
@@ -29,5 +35,17 @@ class Person {
 	@Override
 	public String toString() {
 		return String.format("Person(%s, %s, %s)", firstName, lastName, zipCode);
+	}
+	
+	@Override
+	public int compareTo(Person other) {
+		return this.firstName.length() - other.firstName.length();
+	}
+}
+
+class MyComparator implements Comparator<Person> {
+	@Override
+	public int compare(Person p1, Person p2) {
+		return -p1.compareTo(p2);
 	}
 }
