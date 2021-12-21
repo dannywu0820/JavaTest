de90a1da64a3e36757f7e53ad9fd60b477ffe042
DannyWu <livebetterlife0820@gmail.com>
2021-12-21 11:30:24 +0800

    [feature] calculate sum with synchronization in block level for both instance methods and static methods

--- src/main/java/idv/ktw/syntax/thread/SynchronizedPractice.java
+++ src/main/java/idv/ktw/syntax/thread/SynchronizedPractice.java
@@ -7,6 +7,7 @@ import java.util.stream.IntStream;
 
 public class SynchronizedPractice {
 	private int sum = 0;
+	private int sum2 = 0;
 	private static int staticSum = 0;
 	
 	public static void main(String[] args) {
@@ -14,7 +15,7 @@ public class SynchronizedPractice {
 		SynchronizedPractice summation = new SynchronizedPractice();
 		
 		try {
-			IntStream.range(0, 1000).forEach(count -> service.submit(SynchronizedPractice::calculateStatic));
+			IntStream.range(0, 1000).forEach(count -> service.submit(summation::doSyncTaskStatic));
 			service.awaitTermination(1000, TimeUnit.MILLISECONDS);
 		}
 		catch (InterruptedException e) {
@@ -37,11 +38,35 @@ public class SynchronizedPractice {
 		staticSum += 1;
 	} 
 	
+	public void doSyncTask() {
+		// "this" refers to the monitor object, which is passed into sync block
+		// only one thread per monitor object can execute the code inside the block
+		synchronized (this) { 
+			setSum(getSum() + 1);
+		}
+		setSum2(getSum2() + 1);
+		//this.sum2 += 1;
+	}
+	
+	public void doSyncTaskStatic() {
+		synchronized (SynchronizedPractice.class) { // let class become the monitor
+			staticSum += 1;
+		}
+	}
+	
 	public int getSum() {
 		return this.sum;
 	}
 	
 	public void setSum(int value) {
 		this.sum = value;
+	}
+	
+	public int getSum2() {
+		return this.sum2;
+	}
+	
+	public void setSum2(int value) {
+		this.sum2 = value;
 	}
 }
