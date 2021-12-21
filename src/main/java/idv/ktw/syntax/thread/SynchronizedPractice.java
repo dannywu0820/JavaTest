a1c7630f257dc59295b2b8f932526e8fd2431a10
DannyWu <livebetterlife0820@gmail.com>
2021-12-21 11:05:13 +0800

    [feature] calculate sum without synchronization

---
+++ src/main/java/idv/ktw/syntax/thread/SynchronizedPractice.java
@@ -0,0 +1,38 @@
+package idv.ktw.syntax.thread;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.IntStream;
+
+public class SynchronizedPractice {
+	private int sum = 0;
+	
+	public static void main(String[] args) {
+		ExecutorService service = Executors.newFixedThreadPool(3);
+		SynchronizedPractice summation = new SynchronizedPractice();
+		
+		try {
+			IntStream.range(0, 1000).forEach(count -> service.submit(summation::calculate));
+			service.awaitTermination(1000, TimeUnit.MILLISECONDS);
+		}
+		catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		finally {
+			System.out.println(summation.getSum());
+		}
+	}
+	
+	public void calculate() {
+		setSum(getSum() + 1);
+	}
+	
+	public int getSum() {
+		return this.sum;
+	}
+	
+	public void setSum(int value) {
+		this.sum = value;
+	}
+}
