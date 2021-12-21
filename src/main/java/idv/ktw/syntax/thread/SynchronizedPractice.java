afd8699f6f091b30c4f30e768bee4f07ee126242
DannyWu <livebetterlife0820@gmail.com>
2021-12-21 11:11:41 +0800

    [feature] calculate sum with synchronization in static-method level

--- src/main/java/idv/ktw/syntax/thread/SynchronizedPractice.java
+++ src/main/java/idv/ktw/syntax/thread/SynchronizedPractice.java
@@ -7,20 +7,21 @@ import java.util.stream.IntStream;
 
 public class SynchronizedPractice {
 	private int sum = 0;
+	private static int staticSum = 0;
 	
 	public static void main(String[] args) {
 		ExecutorService service = Executors.newFixedThreadPool(3);
 		SynchronizedPractice summation = new SynchronizedPractice();
 		
 		try {
-			IntStream.range(0, 1000).forEach(count -> service.submit(summation::calculateSync));
+			IntStream.range(0, 1000).forEach(count -> service.submit(SynchronizedPractice::calculateStatic));
 			service.awaitTermination(1000, TimeUnit.MILLISECONDS);
 		}
 		catch (InterruptedException e) {
 			e.printStackTrace();
 		}
 		finally {
-			System.out.println(summation.getSum());
+			System.out.println(SynchronizedPractice.staticSum);
 		}
 	}
 	
@@ -31,6 +32,10 @@ public class SynchronizedPractice {
 	public synchronized void calculateSync() {
 		setSum(getSum() + 1);
 	}
+	
+	public synchronized static void calculateStatic() {
+		staticSum += 1;
+	} 
 	
 	public int getSum() {
 		return this.sum;
