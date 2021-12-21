7e0ca29382cf138464b7042015a986515dbaad8c
DannyWu <livebetterlife0820@gmail.com>
2021-12-21 16:17:53 +0800

    [feature] learn how to get Future result with isDone() and get()

--- src/main/java/idv/ktw/syntax/thread/FuturePractice.java
+++ src/main/java/idv/ktw/syntax/thread/FuturePractice.java
@@ -1,8 +1,11 @@
 package idv.ktw.syntax.thread;
 
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 /*
  * Future is suitable for the following scenarios:
@@ -12,12 +15,31 @@ import java.util.concurrent.Future;
  */
 
 public class FuturePractice {
-
+	public static void main(String[] args) {
+		demoGetFuture();
+	}
+	
+	static void demoGetFuture() {
+		Future<Integer> future = new SquareCalculator().calculateSquare(25);
+		
+		try {
+			while(!future.isDone()) {
+				System.out.println("Calculating...");
+				Thread.sleep(300);
+			}
+			
+			Integer result = future.get(); 
+			System.out.println(result);
+		} 
+		catch(InterruptedException | ExecutionException e) {
+			e.printStackTrace();
+		}
+	}
 }
 
 class SquareCalculator {
 	private ExecutorService executor = Executors.newSingleThreadExecutor();
-	public static final int SLEEP_SECONDS = 1;
+	public static final int SLEEP_SECONDS = 3;
 	
 	public Future<Integer> calculateSquare(Integer value) {
 		return executor.submit(() -> {
