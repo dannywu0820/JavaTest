f6e9682aed88714df4b0395be553d69f02451c14
DannyWu <livebetterlife0820@gmail.com>
2021-12-28 18:09:10 +0800

    [feature] construct LotteryNumber with inner class Builder

--- src/main/java/idv/ktw/test/LotteryNumber.java
+++ src/main/java/idv/ktw/test/LotteryNumber.java
@@ -7,14 +7,49 @@ import java.util.List;
 import java.util.Set;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder;
 
 import idv.ktw.test.MyNumber.IllegalDuplicateException;
 import idv.ktw.test.MyNumber.IllegalNumberException;
 
+@JsonDeserialize(builder = LotteryNumber.Builder.class)
 public class LotteryNumber {
-	private String no;
-	private List<Integer> number;
-	private int specialNumber;
+	private final String no;
+	private final List<Integer> number;
+	private final int specialNumber;
+	
+	private LotteryNumber(String no, List<Integer> number, int specialNumber) {
+		this.no = no;
+		this.number = number;
+		this.specialNumber = specialNumber;
+	}
+	
+	@JsonPOJOBuilder(withPrefix = "")
+	public static final class Builder {
+		private String no;
+		private List<Integer> number;
+		private int specialNumber;
+		
+		public Builder no(String no) {
+			this.no = no;
+			return this;
+		}
+		
+		public Builder number(List<Integer> number) {
+			this.number = number;
+			return this;
+		}
+		
+		public Builder specialNumber(int specialNumber) {
+			this.specialNumber = specialNumber;
+			return this;
+		}
+		
+		public LotteryNumber build() {
+			return new LotteryNumber(no, number, specialNumber);
+		}
+	}
 	
 	public String getNo() {
 		return this.no;
